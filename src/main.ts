5a7228ac3e2807ac1ce02c248aaab16e76653f65
PrA-GYaN <pragyan1516@gmail.com>
2025-12-02 10:27:27 +0545

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-// import * as multer from 'multer';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Configure multer for file uploads
-  // app.use(multer({ dest: './uploads/temp' }).any());
-  
+
+  // Get the ConfigService instance
+  const configService = app.get(ConfigService);
+
+  // Get the CORS_ORIGIN from environment variables
+  const corsOrigin = configService.get<string>('CORS_ORIGIN', '*'); // Default to '*' if not set
+
   // Enable validation pipes globally
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -16,8 +19,13 @@ async function bootstrap() {
     transform: true,
   }));
 
-  // Enable CORS
-  app.enableCors();
+  // Enable CORS with dynamic origin from the .env file
+  app.enableCors({
+    origin: corsOrigin,
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
